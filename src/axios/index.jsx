ba4efebc2381a84a852cf2a6b88cc51c93be001e
mohdkamleen <kamleenmohd@gmail.com>
2023-10-31 05:40:02 +0530

    adf

--- src/axios/index.jsx
+++ src/axios/index.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';  
 
 export default axios.create({ 
-  baseURL: "https://adengage-task-bk.onrender.com/api", //for production mode 
+  baseURL: "https://sic-bk.onrender.com/api", //for production mode 
   // baseURL: "http://localhost:8000/api" //for development mode
 });
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,29 +1,42 @@
-import { Button, Input, Spin, message } from 'antd'
+import { Button, Input, Segmented, Spin, message } from 'antd'
 import axios from 'axios'
 import React, { useState } from 'react'
 
 const Index = () => {
 
+  const [arealoading, setarealoading] = useState(false)
   const [loading, setloading] = useState(false)
   const [uploadLoading, setUploadLoading] = useState(false)
+  const [area, setarea] = useState([])
   const [data, setdata] = useState({
     image: "",
-    note: "",
+    note: "500",
     address: "",
     pincode: ""
   })
 
   const handleAdd = () => {
-    if (!data.image) return message.warning("Image is required.")
+    // if (!data.image) return message.warning("Image is required.")
     if (!data.note) return message.warning("Note type is required.")
     if (!data.pincode) return message.warning("Pincode is required.")
     setloading(true)
-    axios.post(`/image/add`, data)
+    axios.post(`https://adengage-task-bk.onrender.com/api/image/add`, data)
       .then(e => {
         message.success("Success.")
       })
       .catch(e => console.log(e))
       .finally(e => setloading(false))
+  }
+
+  const handlePincode = (e) => {
+    setdata({ ...data, pincode: e.target.value })
+    // if (e.target.length == 6) {
+      setarealoading(true)
+      axios.get(`https://api.postalpincode.in/pincode/${data.pincode}`)
+        .then(e => setarea(e?.data[0]?.PostOffice))
+        .catch(err => console.log(err))
+        .finally(e => setarealoading(false))
+    // }
   }
 
   return (
@@ -40,7 +53,7 @@ const Index = () => {
       </nav> <br />
 
       <label>
-        Upload File :&ensp; {uploadLoading && <Spin size='small' spinning={true} />} <br />
+        Upload File : &ensp; {uploadLoading && <Spin size='small' spinning={true} />} <br />
         <Input
           accept='image/*'
           id='image' type="file"
@@ -49,28 +62,38 @@ const Index = () => {
             setUploadLoading(true);
             var d = new FormData();
             d.append("file", e.target.files[0])
-            var res = await axios.post("/image/upload", d);
+            var res = await axios.post("https://adengage-task-bk.onrender.com/api/image/upload", d);
             setdata({ ...data, image: res.data?.path })
             setUploadLoading(false);
           }} />
       </label> <br /><br />
 
+      Note :
+      <Segmented
+        options={[500, 2000]}
+        default={500}
+        onChange={e => setloading({ ...data, note: e })}
+      /> <br /><br />
+
       <label>
-        Note : <label>500 <input onChange={e => setloading({ ...data, note: e.target.value })} value={500} type="radio" name='note' /></label> &ensp;
-        <label>2000 <input type="radio" name='note' onChange={e => setloading({ ...data, note: e.target.value })} value={2000} /></label>
+        Pincode : &ensp; {arealoading && <Spin size='small' spinning={true} />}  <br /> <Input onChange={handlePincode} placeholder='eg. 346005' type="number" style={{ width: "200px" }} />
       </label><br /><br />
 
       <label>
-        Address : <br /> <Input.TextArea onChange={e => setloading({ ...data, address: e.target.value })} placeholder='eg. Lucknow IN' type="text" style={{ width: "200px" }} />
-      </label><br /><br />
-      {
-        data.image
-      }
-      <label>
-        Pincode : <br /> <Input placeholder='eg. 346005' type="number" style={{ width: "200px" }} />
+        Address : <br /> <Input.TextArea value={data?.address} onChange={e => setloading({ ...data, address: e.target.value })} placeholder='eg. Lucknow IN' type="text" style={{ width: "200px" }} />
       </label><br /><br />
 
-      <Button onClick={handleAdd}>Submit</Button>
+
+      <ul style={{ margin: "0 5%", cursor: "pointer", display: "flex", gap: "5px", flexDirection: "column" }} >
+        {
+          area?.length > 0 && area.map(e => (
+            <li onClick={j => setdata({ ...data, address: `${e?.Name}, ${e?.District} ${e?.State}, ${e?.Country} (${e?.Pincode})` })} key={e?.Name}>{e?.Name}, {e?.District} {e?.State}, {e?.Country} ({e?.Pincode}) </li>
+          ))
+        }
+      </ul>
+      <br />
+
+      <Button onClick={handleAdd} loading={loading}>Submit</Button>
 
     </div>
   )
